4c1413e9257b484623c71bfab0391fe905ff089d
José Flores <josecruzflores.fl@gmail.com>
2019-05-12 12:48:58 -0500

    Show taxi info in FragmentAccount

--- app/src/main/java/com/example/taxiunico_lespaundras_taxista/FragmentAccount.kt
+++ app/src/main/java/com/example/taxiunico_lespaundras_taxista/FragmentAccount.kt
@@ -1,13 +1,31 @@
 package com.example.taxiunico_lespaundras_taxista
 
+import ViewModels.UserViewModel
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_account.*
+import java.lang.Exception
 
 class FragmentAccount : Fragment() {
+
+    lateinit var model: UserViewModel
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_account, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        model = activity?.run {
+            ViewModelProviders.of(this).get(UserViewModel::class.java)
+        } ?: throw Exception("Invalid activity")
+        account_name.text = model.taxi?.driverName
+        account_plate.text = model.taxi?.plate
+        account_rating.text = model.taxi?.rating.toString()
+        account_email.text = model.taxi?.email
     }
 }
